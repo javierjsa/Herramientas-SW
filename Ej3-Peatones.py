69d1c0ef5f5d752e5f4a36f7bc9f94c3b969cadd
JSA <javierjsa@gmail.com>
2016-11-14 12:16:53 +0100

    Deteccion HOG y salva video

--- Ej3-Peatones.py
+++ Ej3-Peatones.py
@@ -2,8 +2,6 @@ import cv2
 import numpy as np
 import glob
 import argparse
-import imutils
-
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--images", required = False,
@@ -14,23 +12,26 @@ help = "video de salida")
 args = vars(ap.parse_args())
 
 entrada = args['images']
-out=args['out']
+out = args['out']
 
-video=cv2.VideoWriter(out,fourcc=-1,fps=24,frameSize=(640,480))
+code = cv2.VideoWriter_fourcc(*'H264')
+video=cv2.VideoWriter(out, fourcc=code, fps=24, frameSize=(640, 480), isColor=True)
 
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 nombre = entrada + '/*.png'
+color=np.array([(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 255, 255)])
 for name in sorted(glob.glob(nombre)):
-    img=cv2.imread(name)
+    img = cv2.imread(name)
     print name
     rects, weights = hog.detectMultiScale(img, winStride=(8,8), padding=(32,32), scale=1.05)
+    i = 0
     for rec in rects:
-        cv2.rectangle(img,(rec[0],rec[1]),(rec[0]+rec[2],rec[1]+rec[3]),color=(0,255,0),thickness=2)
-    print weights
-    print "\n"
-    cv2.imshow('peatones',img)
+        cv2.rectangle(img,(rec[0],rec[1]),(rec[0]+rec[2],rec[1]+rec[3]), color=color[i % 4], thickness=2)
+        i += 1
+    cv2.imshow('Peatones HOG', img)
     video.write(img)
     cv2.waitKey(1)
 
-video.release
+video.release()
+cv2.destroyAllWindows()
