15c1449dcf794d2fbf37f486fd94c4cb4a48902d
JSA <javierjsa@gmail.com>
2016-11-14 12:46:43 +0100

    Salvado video no funcion

--- Ej2-OpenCV.py
+++ Ej2-OpenCV.py
@@ -1,5 +1,4 @@
 import cv2
-
 import argparse
 import numpy as np
 # Javier Saez Alonso
@@ -18,24 +17,12 @@ origen = args['video']
 destino =args['out']
 
 cap = cv2.VideoCapture(origen)
-#fourcc = cap.get(cv.CV_CAP_PROP_FOURCC)
-fourcc = cap.get(int(6))
-print
+
 ret, frame = cap.read()
+shape= np.shape(frame)
 
-shape=np.shape(frame)
-
-print(shape)
-
-#fourcc= cv2.VideoWriter_fourcc(*'XVID')
-#fourcc = cv2.VideoWriter_fourcc(*'H264')
-out = cv2.VideoWriter(destino, int(fourcc), 24.0, (shape[0], shape[1]), True)
-#out = cv2.VideoWriter(destino, 0x21, 23.98, (640,266), True)
-#fourcc= cv2.VideoWriter_fourcc('X', 'V', 'I','D')
-
-#out = cv2.VideoWriter('avengers-flipped.avi',fourcc, 33.0, (640,480))
-#out = cv2.VideoWriter('prueba.avi',-1, 33.0, (640,480))
-
+code = cv2.VideoWriter_fourcc(*'H264')
+out = cv2.VideoWriter(destino, fourcc=code, fps=24.0, frameSize=(shape[0], shape[1]),isColor=True)
 
 #el detector de ojos solamente recibe las regiones donde se ha detectado una cara
 faceCascade = cv2.CascadeClassifier("./haarcascade_frontalface_default.xml")
@@ -48,27 +35,28 @@ eyeCascade = cv2.CascadeClassifier("./haarcascade_eye.xml")
 (minc,mind)=int(round(shape[1]*0.002)), int(round(shape[2]*0.002))
 
 while(cap.isOpened()):
+    if ret:
+      gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+      rectangulos = faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=20, minSize=(mina,minb))
+      i=0
+      while(i<len(rectangulos)):
+        rects=rectangulos[i]
+        cv2.rectangle(frame, (rects[0],rects[1]), (rects[0]+rects[2],rects[1]+rects[3]), (0, 255, 0), 1)
+        roi=gray[rects[1]:rects[1]+rects[3],rects[0]:rects[0]+rects[2]]
+        ojos = eyeCascade.detectMultiScale(roi, scaleFactor=1.1, minNeighbors=15, minSize=(minc,mind), flags = cv2.CASCADE_SCALE_IMAGE)
+        j=0
+        while(j<len(ojos)):
+          rects_2=ojos[j]
+          cv2.rectangle(frame,(rects[0]+rects_2[0],rects[1]+rects_2[1]), (rects[0]+rects_2[0]+rects_2[2],rects[1]+rects_2[1]+rects_2[3]),(0, 0, 255), 1)
+          j=j+1
+        i=i+1
+      cv2.imshow('frame',frame)
+      if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
 
-  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-  rectangulos = faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=20, minSize=(mina,minb))
-  i=0
-  while(i<len(rectangulos)):
-    rects=rectangulos[i]    
-    cv2.rectangle(frame, (rects[0],rects[1]), (rects[0]+rects[2],rects[1]+rects[3]), (0, 255, 0), 1)
-    roi=gray[rects[1]:rects[1]+rects[3],rects[0]:rects[0]+rects[2]]
-    ojos = eyeCascade.detectMultiScale(roi, scaleFactor=1.1, minNeighbors=15, minSize=(minc,mind), flags = cv2.CASCADE_SCALE_IMAGE)
-    j=0
-    while(j<len(ojos)):
-      rects_2=ojos[j]    
-      cv2.rectangle(frame,(rects[0]+rects_2[0],rects[1]+rects_2[1]), (rects[0]+rects_2[0]+rects_2[2],rects[1]+rects_2[1]+rects_2[3]),(0, 0, 255), 1)  
-      j=j+1
-    i=i+1
-  cv2.imshow('frame',frame)
-  if cv2.waitKey(20) & 0xFF == ord('q'):
-    break
+      out.write(frame)
 
-  out.write(frame)
-  ret, frame = cap.read()
+    ret, frame = cap.read()
 
 out.release()
 cap.release()
