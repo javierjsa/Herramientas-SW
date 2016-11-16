427c2a133c366c8eaeb076c836e0794f16f2c57c
JSA <javierjsa@gmail.com>
2016-11-16 12:51:44 +0100

    Sale si ret=False

--- Ej2-OpenCV.py
+++ Ej2-OpenCV.py
@@ -20,7 +20,7 @@ cap = cv2.VideoCapture(origen)
 
 ret, frame = cap.read()
 shape= np.shape(frame)
-
+print shape
 code = cv2.VideoWriter_fourcc(*'H264')
 out = cv2.VideoWriter(destino, fourcc=code, fps=24.0, frameSize=(shape[0], shape[1]))
 
@@ -35,31 +35,31 @@ eyeCascade = cv2.CascadeClassifier("./haarcascade_eye.xml")
 (minc,mind)=int(round(shape[1]*0.002)), int(round(shape[2]*0.002))
 watchdog=0
 while(cap.isOpened() & watchdog<100):
-    if ret:
-      watchdog=0
-      gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-      rectangulos = faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=20, minSize=(mina,minb))
-      i=0
-      while(i<len(rectangulos)):
-        rects=rectangulos[i]
-        cv2.rectangle(frame, (rects[0],rects[1]), (rects[0]+rects[2],rects[1]+rects[3]), (0, 255, 0), 1)
-        roi=gray[rects[1]:rects[1]+rects[3],rects[0]:rects[0]+rects[2]]
-        ojos = eyeCascade.detectMultiScale(roi, scaleFactor=1.1, minNeighbors=15, minSize=(minc,mind), flags = cv2.CASCADE_SCALE_IMAGE)
-        j=0
-        while(j<len(ojos)):
-          rects_2=ojos[j]
-          cv2.rectangle(frame,(rects[0]+rects_2[0],rects[1]+rects_2[1]), (rects[0]+rects_2[0]+rects_2[2],rects[1]+rects_2[1]+rects_2[3]),(0, 0, 255), 1)
-          j=j+1
-        i=i+1
-      cv2.imshow('frame',frame)
-      if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
 
-      out.write(frame)
-    else:
-      watchdog+=1
-    ret, frame = cap.read()
+  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+  rectangulos = faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=20, minSize=(mina,minb))
+  i=0
+  while(i<len(rectangulos)):
+    rects=rectangulos[i]
+    cv2.rectangle(frame, (rects[0],rects[1]), (rects[0]+rects[2],rects[1]+rects[3]), (0, 255, 0), 1)
+    roi=gray[rects[1]:rects[1]+rects[3],rects[0]:rects[0]+rects[2]]
+    ojos = eyeCascade.detectMultiScale(roi, scaleFactor=1.1, minNeighbors=15, minSize=(minc,mind), flags = cv2.CASCADE_SCALE_IMAGE)
+    j=0
+    while(j<len(ojos)):
+      rects_2=ojos[j]
+      cv2.rectangle(frame,(rects[0]+rects_2[0],rects[1]+rects_2[1]), (rects[0]+rects_2[0]+rects_2[2],rects[1]+rects_2[1]+rects_2[3]),(0, 0, 255), 1)
+      j=j+1
+    i=i+1
+  cv2.imshow('frame',frame)
+  if cv2.waitKey(20) & 0xFF == ord('q'):
+    break
 
+  out.write(frame)
+
+  ret, frame = cap.read()
+  if ret == False:
+      print("No se pueden recuperar mas frames")
+      break
 out.release()
 cap.release()
 cv2.destroyAllWindows()
