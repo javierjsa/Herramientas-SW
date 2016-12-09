7684bab2ebde2b3b6cb39ed83b69c7a2079ce7d7
JSA <javierjsa@gmail.com>
2016-12-09 19:00:47 +0100

    Rotación y detección correcta

--- barCodeDetector.m
+++ barCodeDetector.m
@@ -20,47 +20,63 @@ for i=1:4
     im =imclose(im,ses);
 end
 
-prop=regionprops(im,'Area','BoundingBox','Orientation');
+prop=regionprops(im,'Area','BoundingBox','Orientation','Extrema');
 
 aux=prop(1);
 for i=2:length(prop)
     a=prop(i);
     if a.Area>aux.Area
-        aux=a;
+        aux=a;      
     end    
 end    
+disp(i);
+%https://es.mathworks.com/matlabcentral/answers/97460-why-does-the-regionprops-command-not-return-the-correct-orientation-for-a-square-region
+sides = a.Extrema(4,:) - a.Extrema(6,:);
+angle = atan(-sides(2)/sides(1));
 
-cont=a.BoundingBox;
-angle=a.Orientation;
+if (abs(angle-1.5708)<0.174533 || abs(angle-0)<0.174533)
+    angle=0;
+end
 
-rot=[cos(angle) -sin(angle); sin(angle) cos(angle)];
+cont=aux.BoundingBox;
 
-
-a=[0 cont(4)]; %esquina inf izda
-b=[cont(3) cont(4)];%esquina inf dcha
-c=[cont(3) 0]; %esquina sup dcha;
-
-a=rot*a';
-b=rot*b';
-c=rot*c';
-
-x=[cont(1) cont(2)];
-a=a'+ x;
-b=b'+ x;
-c=c'+ x;
+x=cont(1);
+y=cont(2);
+w=cont(3);
+h=cont(4);
 
 
+a=[x y]; %esquina inf. izda
+b=[x y+h]; %esquina sup izda
+c=[(x+w) (y+h)];%esquina sup dcha
+d=[(x+w) y]; %esquina inf dcha;
 
-vertx=[x(1),a(1),b(1),c(1)];
-verty=[x(2),a(1),b(2),c(2)];
+centro = [ (c(1)+b(1))/2  (d(2)+c(2))/2  ];
 
+R = [cos(angle) -sin(angle); sin(angle) cos(angle)];
+
+ca=a-centro;
+cb=b-centro;
+cc=c-centro;
+cd=d-centro;
+
+ra=ca*R;
+rb=cb*R;
+rc=cc*R;
+rd=cd*R;
+
+fa=ra+centro;
+fb=rb+centro;
+fc=rc+centro;
+fd=rd+centro;
+
+x=round([fa(1) fb(1) fc(1) fd(1) fa(1)]);
+y=round([fa(2) fb(2) fc(2) fd(2) fa(2)]);
+
+figure(2);
 imshow(imr);
 hold on;
-plot(vertx,verty,'.-');
-%rectangle('Position',cont,'EdgeColor','g','LineWidth',2);
+plot(x,y,'.-','Color',[1,0,0]);
 
 
-
-
-end
 
